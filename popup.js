3f4d7c0cd5a0869c7a4ad0696389a3f577b2aeb2
Komal Vachhani <komal.vachhani@outlook.com>
2023-02-11 14:23:27 -0500

    ui changes init

--- popup.js
+++ popup.js
@@ -12,11 +12,25 @@ chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
   OpenaiFetchAPI(url);
 });
 
+document.getElementById("showMore").addEventListener("click", function() {
+  let boxes = document.querySelectorAll("#box");
+  for (let i = 3; i < boxes.length; i++) {
+    boxes[i].style.display = "block"; 
+  }
+  this.style.display = "none";
+});
+
+var url = "https://google.com";
+
+document.getElementById("button").addEventListener("click", function() {
+  chrome.tabs.create({ url: url });
+});
+
 function OpenaiFetchAPI(url) {
   console.log("Calling GPT3");
   var url = "https://api.openai.com/v1/engines/davinci/completions";
   var bearer =
-    "Bearer " + "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";
+    "Bearer " + "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";
   fetch(url, {
     method: "POST",
     headers: {
@@ -24,8 +38,8 @@ function OpenaiFetchAPI(url) {
       "Content-Type": "application/json",
     },
     body: JSON.stringify({
-      prompt: "write me a summary of this article: " + url,
-      max_tokens: 300,
+      prompt: "write me a poem",
+      max_tokens: 64,
       temperature: 0.5,
       top_p: 1,
       n: 1,
